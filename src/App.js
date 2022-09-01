b4326c96722a0b36abbd070a9a1878f8d8b3496a
Carolyn Hudson <96785153+cghudson@users.noreply.github.com>
2022-09-01 17:03:42 -0500

    add dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import { createContext } from "react";
 import Footer from "./components/footer";
 import Navigation from "./components/navigation";
 import About from "./components/about";
 import Contact from "./components/contact";
 import Resume from "./components/resume";
 import Portfolio from "./components/portfolio";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
 
 function App() {
   const pages = [
@@ -27,24 +31,35 @@ function App() {
   ];
 
   const [currentPage, setCurrentPage] = useState(pages[0]);
-  
+
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((current) => (current === "light" ? "dark" : "light"));
+  };
 
   return (
-    <>
-      <Navigation
-        pages={pages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      ></Navigation>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme}>
+        <Navigation
+          pages={pages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        ></Navigation>
 
-      <main>
-        {currentPage.name === "About" && <About></About>}
-        {currentPage.name === "Portfolio" && <Portfolio></Portfolio>}
-        {currentPage.name === "Resume" && <Resume></Resume>}
-        {currentPage.name === "Contact" && <Contact></Contact>}
-      </main>
-      <Footer />
-    </>
+        <main>
+          <div className="switch">
+            <label> {theme === "light" ? "Light Mode" : "Dark Mode"} </label>
+            <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+          </div>
+          {currentPage.name === "About" && <About></About>}
+          {currentPage.name === "Portfolio" && <Portfolio></Portfolio>}
+          {currentPage.name === "Resume" && <Resume></Resume>}
+          {currentPage.name === "Contact" && <Contact></Contact>}
+        </main>
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
